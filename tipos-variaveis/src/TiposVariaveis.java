6098bfe4ac2a63030d9b09006501c88771c0ea7e
Mateus_Rodrigues <59971705+Mateus-Rodrigues@users.noreply.github.com>
2025-03-25 14:33:55 -0300

    atualização do curso
    programa simples para testar o aprendizado no inicio do curso

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String meuNome = "Mateus";
+        String meuSobrenome = "Rodrigues";
+
+        int ano_De_Nascimento = 1993;
+        int ano_Atual = 2025;
+
+        int idade = ano_Atual - ano_De_Nascimento;
+
+        System.out.print("Olá " + meuNome + (" ") + meuSobrenome + " parabens pelos seus " + idade + " anos");
     }
 }
